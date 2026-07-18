f1d9f89feef1879adf1b4d90dfd66d00368f6a2e
unknown <juliano.martins81000@gmail.com>
2026-07-18 23:43:58 +0200

    Custom error page

--- app/__init__.py
+++ app/__init__.py
@@ -50,4 +50,18 @@ def create_app(config_class=Config):
             return {"unread_notifications_count": Notification.count_unread(current_user.id)}
         return {"unread_notifications_count": 0}
 
+    from flask import render_template
+
+    @app.errorhandler(403)
+    def forbidden_error(error):
+        return render_template("errors/403.html"), 403
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template("errors/404.html"), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return render_template("errors/500.html"), 500
+
     return app
